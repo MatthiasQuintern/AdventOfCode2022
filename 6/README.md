5ef53ceb944550a492b281fd5fc5d9ef9d14b07a
matthias@arch <matthiasqui@protonmail.com>
2022-12-06 19:38:16 +0100

    day6

---
+++ 6/README.md
@@ -0,0 +1,10 @@
+# [Day 6](https://adventofcode.com/2022/day/6) 
+:gift::gift::gift::gift::gift::gift:
+
+Today's language: **HTML** *(javascript)*
+
+### [Click here to see the website in try it out for yourself](https://quintern.xyz/advent22/day6.html)
+
+```shell
+firefox day6.html
+```
---
+++ 6/day6.js
@@ -0,0 +1,60 @@
+let output = document.getElementById("output");
+let input;
+let fileContent;
+
+function readFile(input) {
+    let file = input.files[0]; 
+    console.log("Reading file", file)
+    let fileReader = new FileReader();
+    fileReader.readAsText(file); 
+    fileReader.onload = function() {
+        fileContent = fileReader.result;
+  }; 
+  fileReader.onerror = function() {
+      alert(fileReader.error);
+  }; 
+}
+
+let start = document.getElementById("start");
+start.addEventListener("mouseover", () => {
+    start.innerHTML = "JUST DO IT"
+});
+start.addEventListener("mouseout", () => {
+    start.innerHTML = "Read File"
+});
+start.addEventListener("click", () => {
+    findPosition(fileContent)
+});
+
+
+function findPosition(line) {
+    let i = 4;
+    let packetStart = line.substring(0, 4);  // should always contain 4 chars
+    let messageStart = line.substring(0, 14);  // should always contain 14 chars
+    let packetStartI = 0;
+    while (i < line.length) {
+        console.log(i, "Char", line[i], "chars:", packetStart);
+        // if packetStart not found, look for packet start
+        if (packetStartI == 0) {
+            if (/(.).*\1/.test(packetStart)) {   // if unique
+                packetStart = packetStart.substring(1, 4) + line[i];
+            }
+            else {
+                packetStartI = i;
+                messageStart = line.substring(i, i + 14);
+            }
+        }
+        else {  // search for message start
+            if (/(.).*\1/.test(messageStart)) {   // if unique
+                messageStart = messageStart.substring(1, 14) + line[i];
+            }
+            else {
+              output.innerHTML = "Found the first packet start marker at position <b>" + packetStartI + "</b>, the marker is " + packetStart + "<br>" +
+                  "Found the first message start marker at position <b>" + i + "</b>, the marker is " + messageStart + "\n";
+              return;
+            }
+        }
+        i++;
+    }
+
+}
